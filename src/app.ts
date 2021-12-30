294db7ba13933b5df5c4b69687e93b8f9dab158c
Aleks <a.cikinovs@icloud.com>
2021-12-30 04:14:05 +0200

    Setting Up a Google API Key. Using Axios to Fetch Coordinates for an Entered Address

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-console.log('Google API')
+import axios from "axios";
+
+// selectors from index
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+// key from Google
+const GOOGLE_API_KEY = "AIzaSyCJlzct62Z_9ndCV1hOnEeRBK9ZoW5THag";
+
+// Custom type
+// response type to get lat and lng
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    // Promise with API request and custom type
+    axios
+        .get<GoogleGeocodingResponse>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+                enteredAddress
+            )}&key=${GOOGLE_API_KEY}`
+        )
+        .then(response => {
+            if (response.data.status !== "OK") {
+                throw new Error("Could not fetch location!");
+            }
+            console.log(response);
+            const coordinates = response.data.results[0].geometry.location;
+            console.log(coordinates);
+        })
+        .catch(err => {
+            alert(err.message);
+            console.log(err);
+        });
+}
+
+form.addEventListener("submit", searchAddressHandler);
